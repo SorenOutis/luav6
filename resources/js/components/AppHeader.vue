993797c5028ea727a19f1bd38742a59b55134da6
SorenOutis <poweredbyrazer022@gmail.com>
2026-04-02 13:00:15 +0800

    feat: implement AppHeader and AppSidebar components with theme switching and navigation support

--- resources/js/components/AppHeader.vue
+++ resources/js/components/AppHeader.vue
@@ -122,18 +122,18 @@ const mainNavItems: NavItem[] = [
     },
 ];
 
-const rightNavItems: NavItem[] = [
-    {
-        title: 'Repository',
-        href: 'https://github.com/laravel/vue-starter-kit',
-        icon: Folder,
-    },
-    {
-        title: 'Documentation',
-        href: 'https://laravel.com/docs/starter-kits#vue',
-        icon: BookOpen,
-    },
-];
+// const rightNavItems: NavItem[] = [
+//     {
+//         title: 'Repository',
+//         href: 'https://github.com/laravel/vue-starter-kit',
+//         icon: Folder,
+//     },
+//     {
+//         title: 'Documentation',
+//         href: 'https://laravel.com/docs/starter-kits#vue',
+//         icon: BookOpen,
+//     },
+// ];
 </script>
 
 <template>
