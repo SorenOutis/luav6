d6616880ca09857cfafdf51386c6086ed84d933f
Soren Outis <poweredbyrazer022@gmail.com>
2026-04-06 09:40:58 +0800

    feat: add ExamsTable configuration with CSV import and template download actions

--- app/Filament/Resources/Exams/Tables/ExamsTable.php
+++ app/Filament/Resources/Exams/Tables/ExamsTable.php
@@ -35,8 +35,40 @@ class ExamsTable
             ])
             ->recordActions([
                 EditAction::make(),
+                \Filament\Actions\Action::make('uploadQuestions')
+                    ->label('Import Questions')
+                    ->icon('heroicon-o-arrow-up-tray')
+                    ->color('warning')
+                    ->form([
+                        \Filament\Forms\Components\FileUpload::make('questions_file')
+                            ->label('Select CSV File')
+                            ->required()
+                            ->disk('local')
+                            ->directory('temp-uploads')
+                            ->acceptedFileTypes(['text/csv', 'application/vnd.ms-excel', 'text/plain']),
+                    ])
+                    ->action(function (array $data, \App\Models\Exam $record) {
+                        $file = \Illuminate\Support\Facades\Storage::disk('local')->path($data['questions_file']);
+                        (new \App\Services\ExamTemplateService())->uploadFromCsv($record, $file);
+                        
+                        \Filament\Notifications\Notification::make()
+                            ->title('Questions imported successfully')
+                            ->success()
+                            ->send();
+                    }),
             ])
             ->toolbarActions([
+                \Filament\Actions\Action::make('downloadTemplate')
+                    ->label('Download Template')
+                    ->icon('heroicon-o-arrow-down-tray')
+                    ->color('info')
+                    ->action(function () {
+                        $csv = (new \App\Services\ExamTemplateService())->getTemplateCsv();
+                        return response()->streamDownload(
+                            fn () => print($csv),
+                            'exam-template.csv'
+                        );
+                    }),
                 BulkActionGroup::make([
                     DeleteBulkAction::make(),
                 ]),
