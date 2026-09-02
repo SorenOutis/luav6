f15946a3a3da06bfe6a64e1e29b1387a705429b3
SorenOutis <poweredbyrazer022@gmail.com>
2026-09-03 00:35:07 +0800

    fix(seo): dynamic sitemap with lastmod + tighten robots for private routes
    - SitemapController: add lastmod, absolute loc via e(), consistent trailing slash
    - RobotsController: add Disallow for /library, /chats, /activities, /calendar (private student pages)

--- app/Http/Controllers/SitemapController.php
+++ app/Http/Controllers/SitemapController.php
@@ -9,20 +9,22 @@ class SitemapController extends Controller
     public function __invoke(Request $request)
     {
         $base = rtrim((string) config('app.url'), '/');
+        $now = now()->format('Y-m-d');
 
         $pages = [
-            '/' => ['1.0', 'weekly'],
-            '/about' => ['0.6', 'monthly'],
-            '/how-it-works' => ['0.7', 'monthly'],
+            '/' => ['1.0', 'weekly', $now],
+            '/about' => ['0.6', 'monthly', $now],
+            '/how-it-works' => ['0.7', 'monthly', $now],
         ];
 
         $xml = '<?xml version="1.0" encoding="UTF-8"?>'."\n";
         $xml .= '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">'."\n";
 
-        foreach ($pages as $path => [$priority, $frequency]) {
-            $loc = $path === '/' ? $base : $base.$path;
+        foreach ($pages as $path => [$priority, $frequency, $lastmod]) {
+            $loc = $path === '/' ? $base.'/' : $base.$path;
             $xml .= "  <url>\n";
-            $xml .= "    <loc>{$loc}</loc>\n";
+            $xml .= '    <loc>'.e($loc)."</loc>\n";
+            $xml .= '    <lastmod>'.$lastmod."</lastmod>\n";
             $xml .= "    <changefreq>{$frequency}</changefreq>\n";
             $xml .= "    <priority>{$priority}</priority>\n";
             $xml .= "  </url>\n";
