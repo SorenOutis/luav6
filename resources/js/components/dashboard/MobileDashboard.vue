35028164522bc80cfcf0eacdb655f752b79896b9
SorenOutis <poweredbyrazer022@gmail.com>
2026-09-18 15:48:55 +0800

    Mobile dashboard: drop the Almost-there status line from the greeting

--- resources/js/components/dashboard/MobileDashboard.vue
+++ resources/js/components/dashboard/MobileDashboard.vue
@@ -96,8 +96,6 @@ const props = withDefaults(
         announcements: Announcement[];
         timeBasedGreeting: string;
         greetingTheme: string;
-        statusColor: string;
-        smarterStatus: string;
         isRefreshing: boolean;
         todayTasks: TodayTask[];
         claimXp: ClaimXp;
@@ -289,13 +287,6 @@ const seasonDateLabel = computed(() => {
                                 ></span>
                             </span>
                         </span>
-                        <p class="mobile-dashboard-status">
-                            <span
-                                class="mobile-dashboard-status-dot"
-                                :class="statusColor"
-                            />
-                            <span class="min-w-0">{{ smarterStatus }}</span>
-                        </p>
                     </div>
                 </div>
                 <FoxCompanion
